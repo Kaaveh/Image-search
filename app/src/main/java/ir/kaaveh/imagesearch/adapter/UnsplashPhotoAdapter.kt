c7f545a224d603d8a961f6b5dbc1f8ef0a85a22f
Kaaveh <kaaveh@pm.me>
2020-10-25 10:17:07 +0330

    Setup RecyclerView adapter

---
+++ app/src/main/java/ir/kaaveh/imagesearch/adapter/UnsplashPhotoAdapter.kt
@@ -0,0 +1,53 @@
+package ir.kaaveh.imagesearch.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import ir.kaaveh.imagesearch.databinding.ItemUnsplashPhotoBinding
+import ir.kaaveh.imagesearch.model.UnsplashPhoto
+
+class UnsplashPhotoAdapter :
+    PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter.PhotoViewHolder>(PHOTO_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val binding =
+            ItemUnsplashPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        currentItem?.let {
+            holder.bind(it)
+        }
+    }
+
+    class PhotoViewHolder(private val binding: ItemUnsplashPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(photo: UnsplashPhoto) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(photo.urls.regular)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(imageView)
+
+                textViewUserName.text = photo.user.username
+            }
+        }
+    }
+
+    companion object {
+        private val PHOTO_COMPARATOR = object : DiffUtil.ItemCallback<UnsplashPhoto>() {
+            override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
+                oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/ir/kaaveh/imagesearch/view/GalleryFragment.kt
+++ app/src/main/java/ir/kaaveh/imagesearch/view/GalleryFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import ir.kaaveh.imagesearch.R
+import ir.kaaveh.imagesearch.adapter.UnsplashPhotoAdapter
+import ir.kaaveh.imagesearch.databinding.FragmentGalleryBinding
+import ir.kaaveh.imagesearch.viewmodel.GalleryViewModel
 
 @AndroidEntryPoint
-class GalleryFragment : Fragment() {
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    private val viewModel by viewModels<GalleryViewModel>()
+    private var binding: FragmentGalleryBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentGalleryBinding.bind(view)
+        val adapter = UnsplashPhotoAdapter()
+        binding?.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
+        }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
