5ca477d1a21df27b472dea272e9996c44146f4a8
Kaaveh <kaaveh@pm.me>
2020-10-25 09:30:02 +0330

    Setup Paging mechanism

--- app/src/main/java/ir/kaaveh/imagesearch/repository/UnsplashRepository.kt
+++ app/src/main/java/ir/kaaveh/imagesearch/repository/UnsplashRepository.kt
@@ -1,10 +1,23 @@
 package ir.kaaveh.imagesearch.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import ir.kaaveh.imagesearch.repository.network.UnsplashApi
+import ir.kaaveh.imagesearch.repository.network.UnsplashPagingSource
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
 
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
 }
--- app/src/main/java/ir/kaaveh/imagesearch/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/ir/kaaveh/imagesearch/viewmodel/GalleryViewModel.kt
@@ -1,7 +1,11 @@
 package ir.kaaveh.imagesearch.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import ir.kaaveh.imagesearch.repository.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
@@ -9,4 +13,16 @@ class GalleryViewModel @ViewModelInject constructor(
 ) :
     ViewModel() {
 
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    val photos = currentQuery.switchMap {queryString ->
+        repository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query: String){
+        currentQuery.value = query
+    }
+
+    companion object {
+        private const val DEFAULT_QUERY = "cats"
+    }
 }
